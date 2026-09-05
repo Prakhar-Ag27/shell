394090747c202ebd5d0c2fb079a088c689f4c6de
Prakhar Agrawal <agrawal.prakhar35@gmail.com>
2026-09-05 09:55:37 +0000

    fix odr

--- src/tokenize.hpp
+++ src/tokenize.hpp
@@ -1,17 +1,4 @@
 #include <string>
 #include <vector>
 
-void tokenize_on_spaces(std::string &input, std::vector<std::string> &v) {
-  int i = 0;
-  while (i < input.length()) {
-    std::string curr = "";
-    while (i < input.length() && input[i] != ' ') {
-      curr += input[i];
-      i++;
-    }
-    while (i < input.length() && input[i] == ' ') {
-      i++;
-    }
-    v.emplace_back(curr);
-  }
-}
+void tokenize_on_spaces(std::string &input, std::vector<std::string> &v);
