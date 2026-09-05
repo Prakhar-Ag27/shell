46118bcb681a2932437dd6ebc7d3df4dee0d8de3
Prakhar Agrawal <agrawal.prakhar35@gmail.com>
2026-09-05 08:50:21 +0000

    Add tests and Cmake support with folder segregation
    Add basic setup for gtest using Cmake and add src and
    test folders for better segregation.

---
+++ src/tokenize.hpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+void tokenize_on_spaces(std::string &input, std::vector<std::string> &v) {
+    int i = 0;
+    while(i < input.length()) {
+        std::string curr = "";
+        while(i < input.length() && input[i] != ' ') {
+            curr += input[i];
+            i++;
+        }
+        i++;
+        v.emplace_back(curr);
+    }
+}
