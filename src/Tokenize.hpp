cfc2fe6d1cc7da70720056186bc7f60209599e38
Prakhar Agrawal <96583373+Prakhar-Ag27@users.noreply.github.com>
2026-09-05 21:10:42 +0530

    Lastaattempt (#7)
    * First code commit
    
    * Add tests and Cmake support with folder segregation
    
    Add basic setup for gtest using Cmake and add src and
    test folders for better segregation.
    
    * Add tests and Cmake support with folder segregation
    
    Add basic setup for gtest using Cmake and add src and
    test folders for better segregation.
    
    * fix odr
    
    * Pls God help me
    
    * add basic shell and segregate classes
    
    * case sensitivity
    
    * case sensitivity
    
    * plswork
    
    * plsplswork

---
+++ src/Tokenize.hpp
@@ -0,0 +1,7 @@
+#include <cstddef>
+#include <string>
+
+
+char ** tokenize_on_spaces(std::string &input);
+
+std::size_t numberOfTokens(std::string &input);
--- src/tokenize.hpp
+++
@@ -1,4 +0,0 @@
-#include <string>
-#include <vector>
-
-void tokenize_on_spaces(std::string &input, std::vector<std::string> &v);
