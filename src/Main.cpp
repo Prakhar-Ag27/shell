276ed8f81c0260fa4a146abfcdc9c4083ce363c8
Prakhar Agrawal <agrawal.prakhar35@gmail.com>
2026-09-06 12:14:24 +0000

    FIx bugs

--- src/Main.cpp
+++ src/Main.cpp
@@ -13,15 +13,23 @@ int main() {
     std::cout << getCurrentWorkingDirectory() << " " << shellPrompt << " ";
     std::string input;
     std::getline(std::cin, input, '\n');
-    char * const * tokens = tokenize_on_spaces(input);
+    char *const *tokens = tokenize_on_spaces(input);
     Interceptor interceptor_;
-    if(interceptor_.checkIfSupported(tokens[0])) {
-        interceptor_.intercept(tokens);
-        continue;
+    if (tokens != nullptr && interceptor_.checkIfSupported(tokens[0])) {
+      interceptor_.intercept(tokens);
+      for (size_t i = 0; tokens[i] != nullptr; ++i) {
+        delete[] tokens[i];
+      }
+      delete[] tokens;
+      continue;
     }
     pid_t p_id = fork();
     if (p_id == -1) {
       std::cout << "Shell Error. Please Retry \n";
+      for (size_t i = 0; tokens[i] != nullptr; ++i) {
+        delete[] tokens[i];
+      }
+      delete[] tokens;
     } else if (p_id == 0) {
       shell_.execute(tokenize_on_spaces(input));
       _exit(1);
