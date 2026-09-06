82a7a2ad09a9f1a4d27ea214798cb8f8b2dcfcfd
Prakhar Agrawal <96583373+Prakhar-Ag27@users.noreply.github.com>
2026-09-06 21:56:55 +0530

    Refactor and add bg exec functionality (#12)
    Added an interactive shell session with a configurable $ prompt and current working directory display.
    Added support for entering commands, including recognized built-in commands.
    Added background process handling, allowing commands to run without blocking the shell.
    Added process status reporting, including completion codes and termination signals.
    Added automatic cleanup and waiting for active processes when appropriate.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,48 +1,8 @@
 #include "Shell.hpp"
-#include "Utils.hpp"
-#include <cstdio>
-#include <iostream>
-#include <string>
 #include <sys/wait.h>
 #include <unistd.h>
 
 int main() {
-  char shellPrompt = '$';
-  Shell shell_;
-  while (true) {
-    std::cout << getCurrentWorkingDirectory() << " " << shellPrompt << " ";
-    std::string input;
-    std::getline(std::cin, input, '\n');
-    char *const *tokens = tokenize_on_spaces(input);
-    Interceptor interceptor_;
-    if (tokens != nullptr && interceptor_.checkIfSupported(tokens[0])) {
-      interceptor_.intercept(tokens);
-      for (size_t i = 0; tokens[i] != nullptr; ++i) {
-        delete[] tokens[i];
-      }
-      delete[] tokens;
-      continue;
-    }
-    pid_t p_id = fork();
-    if (p_id == -1) {
-      std::cout << "Shell Error. Please Retry \n";
-    } else if (p_id == 0) {
-      shell_.execute(tokens);
-      _exit(1);
-    } else {
-      int status;
-      waitpid(p_id, &status, 0);
-      if (WIFEXITED(status)) {
-        int exitStatus = WEXITSTATUS(status);
-
-        if (exitStatus != 0) {
-          std::cout << "Process failed with code " << exitStatus << '\n';
-        }
-      }
-    }
-    for (size_t i = 0; tokens[i] != nullptr; ++i) {
-      delete[] tokens[i];
-    }
-    delete[] tokens;
-  }
+  Shell shell_{'$'};
+  shell_.run();
 }
--- src/Shell.hpp
+++ src/Shell.hpp
@@ -1,8 +1,21 @@
-#include "./Tokenize.hpp"
 #include "./Interceptor.hpp"
+#include "./Tokenize.hpp"
+#include <sys/types.h>
 
 class Shell {
-    public:
-        Shell();
-        void execute(char * const *);
+public:
+  Shell();
+  Shell(char);
+  void run();
+
+private:
+  void execute(char *const *);
+  void freeTokens(char *const *);
+  bool checkIfBackgorundProcess(std::string);
+  void waitOnAllProcesses();
+
+  char shellPrompt_;
+  pid_t foregroundProcess_{-1};
+  std::set<pid_t> backgroundProcess_;
+  Interceptor interceptor_;
 };
