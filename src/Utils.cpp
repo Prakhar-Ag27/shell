b556ca18ff57749bde028764228c93caf8342284
Prakhar Agrawal <96583373+Prakhar-Ag27@users.noreply.github.com>
2026-09-06 14:02:24 +0530

    Add CWD functionality and fix exit status handling (#10)
    * Add CWD functionality and fix exit status handling
    
    * Fix review comments

---
+++ src/Utils.cpp
@@ -0,0 +1,23 @@
+#include "Utils.hpp"
+#include <cerrno>
+#include <unistd.h>
+
+std::string getCurrentWorkingDirectory() {
+  size_t size = 1024;
+  char *dir = nullptr;
+  std::string cwd = "";
+  while (dir == nullptr) {
+    char *buf = new char[size];
+    dir = getcwd(buf, size);
+    if (dir == nullptr && errno == ERANGE) {
+      size *= 2;
+    } else if (dir != nullptr) {
+      cwd = dir;
+    } else {
+      delete[] buf;
+      break;
+    }
+    delete[] buf;
+  }
+  return cwd;
+}
