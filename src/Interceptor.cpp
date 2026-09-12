34a28f731acd87f9467749192117688300d1bc0a
Prakhar Agrawal <agrawal.prakhar35@gmail.com>
2026-09-12 22:21:22 +0000

    address review comments

--- src/Interceptor.cpp
+++ src/Interceptor.cpp
@@ -8,7 +8,9 @@ Interceptor::Interceptor(Shell &aShell) : shell_{aShell} {}
 
 bool Interceptor::intercept(const std::vector<std::string> &inputTokens) {
   if (inputTokens[0] == "cd") {
-    if (chdir(inputTokens[1].c_str()) != 0) {
+    if (inputTokens.size() < 2) {
+      std::cout << "Provide the directory to cd into\n";
+    } else if (chdir(inputTokens[1].c_str()) != 0) {
       std::string err = strerror(errno);
       std::cout << "Cannot change the directory with error: " << err << "\n";
     }
--- test/my_tests.cpp
+++ test/my_tests.cpp
@@ -4,29 +4,27 @@
 TEST(TokeizerTest, TokenizeStrings) {
   std::string s{"token1 token2"};
   std::vector<std::string> v = tokenize_on_spaces(s);
-  EXPECT_EQ(v[0], std::string{"token1"});
-  EXPECT_EQ(v[1], "token2");
+  EXPECT_EQ(v, (std::vector<std::string>{"token1", "token2"}));
 }
 
 TEST(TokeizerTest, TokenizeNoStringsWhenInputEmpty) {
   std::string s{""};
   std::vector<std::string> v = tokenize_on_spaces(s);
-  EXPECT_EQ(v.size(), 0);
+  EXPECT_EQ(v, (std::vector<std::string>{}));
 }
 
 TEST(TokeizerTest, TokenizeStringWithOneToken) {
   std::string s{"token1"};
   std::vector<std::string> v = tokenize_on_spaces(s);
-  EXPECT_EQ(v[0], "token1");
+  EXPECT_EQ(v, (std::vector<std::string>{"token1"}));
   std::string t{"token1 "};
   v.clear();
   v = tokenize_on_spaces(t);
-  EXPECT_EQ(v[0], "token1");
+  EXPECT_EQ(v, (std::vector<std::string>{"token1"}));
 }
 
 TEST(TokeizerTest, TokenizeStringWithExtraSpaces) {
   std::string s{"token1  token2  "};
   std::vector<std::string> v = tokenize_on_spaces(s);
-  EXPECT_EQ(v[0], "token1");
-  EXPECT_EQ(v[1], "token2");
+  EXPECT_EQ(v, (std::vector<std::string>{"token1", "token2"}));
 }
