9c99cdc3b9f013897657b3dab9c24bcae3927790
Prakhar Agrawal <agrawal.prakhar35@gmail.com>
2026-09-12 16:09:04 +0000

    Add exit support

--- src/Interceptor.cpp
+++ src/Interceptor.cpp
@@ -3,18 +3,20 @@
 #include <iostream>
 #include <unistd.h>
 
-Interceptor::Interceptor() : supportedOps_{"cd"}{};
+Interceptor::Interceptor() : supportedOps_{"cd", "exit"}{};
 
 bool Interceptor::intercept(char *const *inputTokens) {
   if (strcmp(inputTokens[0], "cd") == 0) {
     if (chdir(inputTokens[1]) != 0) {
       std::string err = strerror(errno);
       std::cout << "Cannot change the directory with error: " << err << "\n";
-      return false;
     }
     return true;
+  } else if (strcmp(inputTokens[0], "exit") == 0) {
+    exit(0);
+    //need not worry about freeing memory here since OS will automatically reclaim the memory
   }
-  return false;
+  return true;
 }
 
 bool Interceptor::checkIfSupported(char *const operation) {
