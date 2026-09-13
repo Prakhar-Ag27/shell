616d0aef9bfee25d04d7a7f922022a587090b37d
Prakhar Agrawal <agrawal.prakhar35@gmail.com>
2026-09-13 17:05:01 +0000

    add support for multi exec mode by introducing parsing an foreground exec

--- src/Shell.hpp
+++ src/Shell.hpp
@@ -16,10 +16,14 @@ private:
   void execute(const std::vector<std::string> &);
   bool checkIfBackgorundProcess(std::string);
   void waitOnAllProcesses();
+  bool checkInput(const std::vector<std::vector<std::string>> &);
+  void executeSingleMode(std::string);
+  void executeMultiMode(const std::vector<std::vector<std::string>> &);
 
   char shellPrompt_;
   pid_t foregroundProcess_{-1};
   std::set<pid_t> backgroundProcess_;
+  std::set<pid_t> foregroundProcessesInMultiMode_;
   Interceptor interceptor_;
   pid_t bgProcessGroup{-1};
 };
--- test/my_tests.cpp
+++ test/my_tests.cpp
@@ -1,3 +1,4 @@
+#include "../src/Parser.hpp"
 #include "../src/Tokenize.hpp"
 #include <gtest/gtest.h>
 
@@ -27,4 +28,38 @@ TEST(TokeizerTest, TokenizeStringWithExtraSpaces) {
   std::string s{"token1  token2  "};
   std::vector<std::string> v = tokenize_on_spaces(s);
   EXPECT_EQ(v, (std::vector<std::string>{"token1", "token2"}));
+}
+
+TEST(ParserTest, ParseMultipleInputs) {
+  std::string input{"tokena1 tokena2 && tokenb1 tokenb2"};
+  std::vector<std::vector<std::string>> vec = parseInput(input);
+  std::vector<std::vector<std::string>> check{{"tokena1 tokena2"},
+                                              {"tokenb1 tokenb2"}};
+  EXPECT_EQ(vec, check);
+}
+
+TEST(ParserTest, ParseMultipleInputsWithMultipleStrings) {
+  std::string input{"tokena1 tokena2 &&& tokena11 tokena12 && tokenb1 tokenb2"};
+  std::vector<std::vector<std::string>> vec = parseInput(input);
+  std::vector<std::vector<std::string>> check{
+      {"tokena1 tokena2", "tokena11 tokena12"}, {"tokenb1 tokenb2"}};
+  EXPECT_EQ(vec, check);
+}
+
+TEST(ParserTest, ParseMultipleInputsWithMultipleStringsExtended) {
+  std::string input{"tokena1 tokena2 &&& tokena11 tokena12 &&& tokena21 "
+                    "tokena22 && tokenb1 tokenb2 &&& tokenb11 tokenb12"};
+  std::vector<std::vector<std::string>> vec = parseInput(input);
+  std::vector<std::vector<std::string>> check{
+      {"tokena1 tokena2", "tokena11 tokena12", "tokena21 tokena22"},
+      {"tokenb1 tokenb2", "tokenb11 tokenb12"}};
+  EXPECT_EQ(vec, check);
+}
+
+TEST(ParserTest, ParseMultipleInputsWithMultipleStringsShort) {
+  std::string input{"tokena1 tokena2 &&& tokena11 tokena12"};
+  std::vector<std::vector<std::string>> vec = parseInput(input);
+  std::vector<std::vector<std::string>> check{
+      {"tokena1 tokena2", "tokena11 tokena12"}};
+  EXPECT_EQ(vec, check);
 }
